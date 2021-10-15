c000e070ed1fa79a0a0fe1cabed7a60005491c35
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-10-15 12:21:34 +0530

    Added python program to find area of circle

--- checkforurl.py
+++ checkforurl.py
@@ -2,7 +2,7 @@ import re #Using the regular expression
 def Lookforurl(string):
     
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.]
-    [a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))
+     [a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))
     +(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)
     |[^\s`!()\[\]{};:'\".,<>?«»""'']))" #Regex checks
     #using findall() and passing input
