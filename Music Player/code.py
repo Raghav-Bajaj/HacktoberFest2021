a019b49975cf98f821cbef2fb2e8f960bfd60b37
Sarthak <sarthak.mishra@s.amity.edu>
2021-10-19 16:36:35 +0530

    Music Player

---
+++ Music Player/code.py
@@ -0,0 +1,5 @@
+from playsound import playsound
+# here we will store song in same folder same as the code
+playsound('Believer.mp3')
+print('Use Headphone for better sound quality')
+print('Faveriote Song')
